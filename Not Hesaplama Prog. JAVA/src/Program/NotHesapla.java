9102b56daf1d3ebe91ade5e3cfef4ff06dc8ff76
Semih Alper Keskin <84216690+semihalperKeskin@users.noreply.github.com>
2022-04-29 13:43:33 +0300

    Add files via upload

---
+++ Not Hesaplama Prog. JAVA/src/Program/NotHesapla.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class NotHesapla {
+    public static void main(String[] args) {
+        double mat,fizik,kimya,tarih,turkce,muzik;
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("Matematik notunuz : ");
+        mat=input.nextDouble();
+        System.out.print("Fizik notunuz : ");
+        fizik=input.nextDouble();
+        System.out.print("Kimya notunuz : ");
+        kimya=input.nextDouble();
+        System.out.print("Tarih notunuz : ");
+        tarih=input.nextDouble();
+        System.out.print("Turkce notunuz : ");
+        turkce=input.nextDouble();
+        System.out.print("Muzik notunuz : ");
+        muzik=input.nextDouble();
+
+        double answer=(mat+fizik+kimya+tarih+muzik+turkce)/6;
+        System.out.println("Not Ortalamaniz: "+answer);
+
+        String PassedOrFail=answer>60 ? "Gecti":"kaldı";
+        System.out.println(PassedOrFail);
+    }
+
+}
